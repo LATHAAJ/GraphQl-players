453b4505790db67c5638eef4eb5a2423d2b3969f
LATHAAJ <122860428+LATHAAJ@users.noreply.github.com>
2025-09-20 15:10:01 +0530

    Revert "GraphQL Cricket Players API"
    This reverts commit 486301ab31b8d6777f5e0e2cdbadf0d7b07aa9cb.

--- graphql-markdown.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  schema: "./src/main/resources/graphql/schema.graphqls", // path to your GraphQL schema
-  output: "./docs",                                      // where Markdown files will be generated
-  loaders: {
-    GraphQLFileLoader: "@graphql-tools/graphql-file-loader"
-  }
-};
